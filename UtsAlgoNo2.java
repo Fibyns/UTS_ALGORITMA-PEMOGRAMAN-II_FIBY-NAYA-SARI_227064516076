5c5e33c374ceb5d35869e7b4c9da8097e4abe365
Fiby Naya Sari <121842510+Fibyns@users.noreply.github.com>
2023-05-23 12:42:55 +0700

    Update UtsAlgoNo2.java

--- UtsAlgoNo2.java
+++ UtsAlgoNo2.java
@@ -1,10 +1,11 @@
+// FIBY NAYA SARI 227064516076
 package utsalgosem2;
 import java.util.Scanner;
 public class UtsAlgoNo2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        System.out.println("## program jav cari nili rata-rata ##");
+        System.out.println("## program jav cari nili rata-rata ##"); //tambahin tanda "" biar kebaca
         System.out.println("======================================");
         System.out.println();
         
@@ -12,16 +13,15 @@ public class UtsAlgoNo2 {
         int arr_count, i;
         float total, rata;
         
-        System.out.print("input jumlah elemen array : "); //sebelumnya System.print gada .out, yg bener System.out.print
-        arr_count = input.nextInt();
+        System.out.print("input jumlah elemen array : "); //salah System.print, seharusnya System.out.print
+        arr_count = input.nextInt(); //tambahin ; diakhir setelah ()
 
         System.out.println("input " + arr_count+ " angka (pisah dgn enter) : ");
         
-        //simpan setiap angka yg di input ke dlm array
+        //simpapn setiap angka yg di input ke dlm array
         for(i=0; i<arr_count; i++){
-            arr[i]=(int) input.nextDouble();
+            arr[i]=input.nextInt(); // salah arr[i]=input.nextDouble();, seharus nya arr[i]=input.nextInt();
         }
-        
         System.out.println();
         
         //cari total semua element array
@@ -29,9 +29,8 @@ public class UtsAlgoNo2 {
         for(i=0; i<arr_count; i++){
             total=total+arr[i];
         }
-        
         //hitung nilai rata2
         rata=(total/arr_count);
-        System.out.println("nilai rata-rata dari " + arr_count + " inputan adalah: "+ rata); //tambahin + sebelum dan sesudah arr_count biar ke baca
+        System.out.println("nilai rata-rata dari " + arr_count + " inputan adalah: "+ rata); //tambahin tanda + sesudah dan sebelum 
     }  
 }
